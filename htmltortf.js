3cfbeefe2f51718f77ae7d5f14843c26909ace67
Steven A. Lowe <Steven A. Lowe>
2016-10-25 15:17:05 -0700

    walks input directory recursively, replicates subfolders to output directory while copying and converting files

--- htmltortf.js
+++ htmltortf.js
@@ -19,27 +19,68 @@ if (typeof String.prototype.endsWith !== 'function') {
     };
 }
 
+//adding in a replaceall string function
+if (typeof String.prototype.replaceAll !== 'function') {
+  String.prototype.replaceAll = function(search, replacement) {
+      var target = this;
+      return target.replace(new RegExp(search, 'g'), replacement);
+  };
+}
+
 const fs = require('fs');
 var cloudconvert = new (require('cloudconvert'))(apikey);
 
 const htmlSuffix = ".html";
-const inPath = inFolder + '/';
-const outPath = outFolder + '/';
+var inPath = inFolder.replaceAll('\\\\','/');
+if (!inPath.endsWith('/')) {
+  inPath = inPath + '/';
+}
 
-fs.readdir(inFolder, (err, files) => {
-  files.forEach(infile => {
-    if (infile.endsWith(htmlSuffix)) {
-        var outfile = outPath + infile.substring(0,infile.length-htmlSuffix.length) + ".rtf";
-        fs.createReadStream(inPath + infile)
-        .pipe(cloudconvert.convert({
-          inputformat: 'html',
-          outputformat: 'rtf'
-        }))
-        .pipe(fs.createWriteStream(outfile))
-        .on('finish', function() {
-          console.log('created ',outfile);
-      });
-    }
-  });
-})
+var outPath = outFolder.replaceAll('\\\\','/');
+if (!outPath.endsWith('/')) {
+  outPath = outPath + '/';
+}
+
+//create a directory if it doesn't exist
+function makeDirectorySync(directory) {  
+  try {
+    fs.statSync(directory);
+  } catch(e) {
+    fs.mkdirSync(directory);
+  }
+}
+
+//scan folder and subfolders; convert .html files to .rtf; copy all other files and folders
+function processDirectory(inFolder,outFolder) {
+  console.log("processDirectory",inFolder,outFolder);
+  fs.readdir(inFolder, (err, files) => {
+    files.forEach(infile => {
+      //recurse if necessary
+      if (fs.statSync(inFolder+infile).isDirectory()) {
+        makeDirectorySync(outFolder+infile);
+        processDirectory(inFolder+infile+"/",outFolder+infile+"/");
+      }
+      //convert html to rtf
+      else if (infile.endsWith(htmlSuffix)) {
+        var outfile = outFolder + infile.substring(0,infile.length-htmlSuffix.length) + ".rtf";
+        fs.createReadStream(inFolder + infile)
+          .pipe(cloudconvert.convert({
+            inputformat: 'html',
+            outputformat: 'rtf'
+          }))
+          .pipe(fs.createWriteStream(outfile))
+          .on('finish', function() {
+            console.log('converted ',outfile);
+          });
+      }
+      //just copy
+      else {
+        console.log("copying ",inFolder+infile);
+        fs.createReadStream(inFolder+infile).pipe(fs.createWriteStream(outFolder+infile));
+      }
+    });
+  })
+}
+
+processDirectory(inPath,outPath);
 
